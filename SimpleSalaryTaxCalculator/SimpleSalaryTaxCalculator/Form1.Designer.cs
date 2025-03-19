4a8a5afa1f93ab2ebb66b410a85b8939d082c5b2
kasra10 <kasrasparrow@gmail.com>
2025-03-19 14:28:53 +0330

    First Files

---
+++ SimpleSalaryTaxCalculator/SimpleSalaryTaxCalculator/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace SimpleSalaryTaxCalculator
+{
+	partial class LoginForm_FRM
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm_FRM));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.UserNameSecret_LBL = new System.Windows.Forms.Label();
+			this.PassWordSecret_LBL = new System.Windows.Forms.Label();
+			this.UserNameTitleSecret_LBL = new System.Windows.Forms.Label();
+			this.PassWordTitleSecret_LBL = new System.Windows.Forms.Label();
+			this.UserNameStar_LBL = new System.Windows.Forms.Label();
+			this.PassWOrdStar_LBL = new System.Windows.Forms.Label();
+			this.ShowKeys_BTN = new System.Windows.Forms.Button();
+			this.UserNameMain_LBL = new System.Windows.Forms.Label();
+			this.UserName_TB = new System.Windows.Forms.TextBox();
+			this.PassWordMain_LBL = new System.Windows.Forms.Label();
+			this.PassWord_TB = new System.Windows.Forms.TextBox();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.panel1.Controls.Add(this.PassWord_TB);
+			this.panel1.Controls.Add(this.PassWordMain_LBL);
+			this.panel1.Controls.Add(this.UserName_TB);
+			this.panel1.Controls.Add(this.UserNameMain_LBL);
+			this.panel1.Location = new System.Drawing.Point(143, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(289, 282);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Location = new System.Drawing.Point(12, 300);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(556, 54);
+			this.panel2.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox1.Image = global::SimpleSalaryTaxCalculator.Properties.Resources.favIcon;
+			this.pictureBox1.Location = new System.Drawing.Point(158, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(31, 32);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(195, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(185, 19);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Powered By Kasra Hosseini-K10";
+			// 
+			// UserNameSecret_LBL
+			// 
+			this.UserNameSecret_LBL.AutoSize = true;
+			this.UserNameSecret_LBL.Location = new System.Drawing.Point(87, 243);
+			this.UserNameSecret_LBL.Name = "UserNameSecret_LBL";
+			this.UserNameSecret_LBL.Size = new System.Drawing.Size(44, 19);
+			this.UserNameSecret_LBL.TabIndex = 2;
+			this.UserNameSecret_LBL.Text = "admin";
+			// 
+			// PassWordSecret_LBL
+			// 
+			this.PassWordSecret_LBL.AutoSize = true;
+			this.PassWordSecret_LBL.Location = new System.Drawing.Point(87, 268);
+			this.PassWordSecret_LBL.Name = "PassWordSecret_LBL";
+			this.PassWordSecret_LBL.Size = new System.Drawing.Size(44, 19);
+			this.PassWordSecret_LBL.TabIndex = 3;
+			this.PassWordSecret_LBL.Text = "admin";
+			// 
+			// UserNameTitleSecret_LBL
+			// 
+			this.UserNameTitleSecret_LBL.AutoSize = true;
+			this.UserNameTitleSecret_LBL.Location = new System.Drawing.Point(12, 243);
+			this.UserNameTitleSecret_LBL.Name = "UserNameTitleSecret_LBL";
+			this.UserNameTitleSecret_LBL.Size = new System.Drawing.Size(69, 19);
+			this.UserNameTitleSecret_LBL.TabIndex = 4;
+			this.UserNameTitleSecret_LBL.Text = "UserName:";
+			// 
+			// PassWordTitleSecret_LBL
+			// 
+			this.PassWordTitleSecret_LBL.AutoSize = true;
+			this.PassWordTitleSecret_LBL.Location = new System.Drawing.Point(12, 267);
+			this.PassWordTitleSecret_LBL.Name = "PassWordTitleSecret_LBL";
+			this.PassWordTitleSecret_LBL.Size = new System.Drawing.Size(68, 19);
+			this.PassWordTitleSecret_LBL.TabIndex = 5;
+			this.PassWordTitleSecret_LBL.Text = "PassWord:";
+			// 
+			// UserNameStar_LBL
+			// 
+			this.UserNameStar_LBL.AutoSize = true;
+			this.UserNameStar_LBL.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.UserNameStar_LBL.Location = new System.Drawing.Point(87, 244);
+			this.UserNameStar_LBL.Name = "UserNameStar_LBL";
+			this.UserNameStar_LBL.Size = new System.Drawing.Size(39, 19);
+			this.UserNameStar_LBL.TabIndex = 6;
+			this.UserNameStar_LBL.Text = "******";
+			// 
+			// PassWOrdStar_LBL
+			// 
+			this.PassWOrdStar_LBL.AutoSize = true;
+			this.PassWOrdStar_LBL.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.PassWOrdStar_LBL.Location = new System.Drawing.Point(87, 270);
+			this.PassWOrdStar_LBL.Name = "PassWOrdStar_LBL";
+			this.PassWOrdStar_LBL.Size = new System.Drawing.Size(39, 19);
+			this.PassWOrdStar_LBL.TabIndex = 7;
+			this.PassWOrdStar_LBL.Text = "******";
+			// 
+			// ShowKeys_BTN
+			// 
+			this.ShowKeys_BTN.BackColor = System.Drawing.Color.Coral;
+			this.ShowKeys_BTN.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.ShowKeys_BTN.Location = new System.Drawing.Point(12, 209);
+			this.ShowKeys_BTN.Name = "ShowKeys_BTN";
+			this.ShowKeys_BTN.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+			this.ShowKeys_BTN.Size = new System.Drawing.Size(119, 30);
+			this.ShowKeys_BTN.TabIndex = 8;
+			this.ShowKeys_BTN.Text = "Show/Hide Info";
+			this.ShowKeys_BTN.UseVisualStyleBackColor = false;
+			this.ShowKeys_BTN.Click += new System.EventHandler(this.ShowKeys_BTN_Click);
+			// 
+			// UserNameMain_LBL
+			// 
+			this.UserNameMain_LBL.AutoSize = true;
+			this.UserNameMain_LBL.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.UserNameMain_LBL.Location = new System.Drawing.Point(23, 63);
+			this.UserNameMain_LBL.Name = "UserNameMain_LBL";
+			this.UserNameMain_LBL.Size = new System.Drawing.Size(66, 19);
+			this.UserNameMain_LBL.TabIndex = 0;
+			this.UserNameMain_LBL.Text = "UserName:";
+			// 
+			// UserName_TB
+			// 
+			this.UserName_TB.BackColor = System.Drawing.SystemColors.Info;
+			this.UserName_TB.Location = new System.Drawing.Point(90, 60);
+			this.UserName_TB.Name = "UserName_TB";
+			this.UserName_TB.Size = new System.Drawing.Size(190, 26);
+			this.UserName_TB.TabIndex = 1;
+			// 
+			// PassWordMain_LBL
+			// 
+			this.PassWordMain_LBL.AutoSize = true;
+			this.PassWordMain_LBL.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.PassWordMain_LBL.Location = new System.Drawing.Point(23, 103);
+			this.PassWordMain_LBL.Name = "PassWordMain_LBL";
+			this.PassWordMain_LBL.Size = new System.Drawing.Size(66, 19);
+			this.PassWordMain_LBL.TabIndex = 2;
+			this.PassWordMain_LBL.Text = "PassWord:";
+			// 
+			// PassWord_TB
+			// 
+			this.PassWord_TB.BackColor = System.Drawing.SystemColors.Info;
+			this.PassWord_TB.Location = new System.Drawing.Point(90, 100);
+			this.PassWord_TB.Name = "PassWord_TB";
+			this.PassWord_TB.PasswordChar = '*';
+			this.PassWord_TB.Size = new System.Drawing.Size(190, 26);
+			this.PassWord_TB.TabIndex = 3;
+			// 
+			// LoginForm_FRM
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(580, 357);
+			this.Controls.Add(this.ShowKeys_BTN);
+			this.Controls.Add(this.PassWOrdStar_LBL);
+			this.Controls.Add(this.UserNameStar_LBL);
+			this.Controls.Add(this.PassWordTitleSecret_LBL);
+			this.Controls.Add(this.UserNameTitleSecret_LBL);
+			this.Controls.Add(this.PassWordSecret_LBL);
+			this.Controls.Add(this.UserNameSecret_LBL);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+			this.Name = "LoginForm_FRM";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Login";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label UserNameSecret_LBL;
+		private System.Windows.Forms.Label PassWordSecret_LBL;
+		private System.Windows.Forms.Label UserNameTitleSecret_LBL;
+		private System.Windows.Forms.Label PassWordTitleSecret_LBL;
+		private System.Windows.Forms.Label UserNameStar_LBL;
+		private System.Windows.Forms.Label PassWOrdStar_LBL;
+		private System.Windows.Forms.Button ShowKeys_BTN;
+		private System.Windows.Forms.TextBox PassWord_TB;
+		private System.Windows.Forms.Label PassWordMain_LBL;
+		private System.Windows.Forms.TextBox UserName_TB;
+		private System.Windows.Forms.Label UserNameMain_LBL;
+	}
+}
+
---
+++ SimpleSalaryTaxCalculator/SimpleSalaryTaxCalculator/Form1.cs
@@ -0,0 +1,52 @@
+﻿// Ignore Spelling: FRM
+
+using System;
+using System.Windows.Forms;
+
+namespace SimpleSalaryTaxCalculator
+{
+	public partial class LoginForm_FRM : Form
+	{
+		public LoginForm_FRM()
+		{
+			InitializeComponent();
+		}
+
+		// Link Image
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			// URL to open 
+			string url = "https://github.com/KASRA10";
+			try
+			{
+				// Use Process.Start to open the URL in the default browser 
+				System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+				{
+					FileName = url,
+					UseShellExecute = true // Ensures it uses the default browser 
+				});
+			}
+			catch (Exception ex)
+			{
+				// Handle exceptions, such as when a browser is unavailable 
+				MessageBox.Show("Unable to open the link. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+			}
+		}
+
+		// Show UserNAme And PassWord
+		private void ShowKeys_BTN_Click(object sender, EventArgs e)
+		{
+			if (UserNameStar_LBL.Visible == true && PassWOrdStar_LBL.Visible == true)
+			{
+				UserNameStar_LBL.Visible = false;
+				PassWOrdStar_LBL.Visible = false;
+			}
+			else
+			{
+				UserNameStar_LBL.Visible = true;
+				PassWOrdStar_LBL.Visible = true;
+			}
+		}
+	}
+}
