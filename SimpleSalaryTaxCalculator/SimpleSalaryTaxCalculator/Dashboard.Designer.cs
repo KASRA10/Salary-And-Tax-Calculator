39316faccc4c8040d8f0c795f4fa19e4af839e42
kasra10 <kasrasparrow@gmail.com>
2025-03-20 14:39:31 +0330

    Complete The Project

--- SimpleSalaryTaxCalculator/SimpleSalaryTaxCalculator/Dashboard.Designer.cs
+++ SimpleSalaryTaxCalculator/SimpleSalaryTaxCalculator/Dashboard.Designer.cs
@@ -30,20 +30,40 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard_FRM));
 			this.BackGround_PNL = new System.Windows.Forms.Panel();
+			this.NetPay_LBL = new System.Windows.Forms.Label();
+			this.NetTitle_LBL = new System.Windows.Forms.Label();
+			this.Marital_COMB = new System.Windows.Forms.ComboBox();
+			this.Reset_BTN = new System.Windows.Forms.Button();
+			this.SalaryCalculation_BTN = new System.Windows.Forms.Button();
+			this.NumberOfCHildren_COMB = new System.Windows.Forms.ComboBox();
+			this.NumberOfChildren_LBL = new System.Windows.Forms.Label();
+			this.MaritalStatus_LBL = new System.Windows.Forms.Label();
+			this.HiringDatePicker_DP = new System.Windows.Forms.DateTimePicker();
+			this.HiringDate_LBL = new System.Windows.Forms.Label();
+			this.NumberOfDays_TB = new System.Windows.Forms.TextBox();
+			this.NumberOfDays_LBL = new System.Windows.Forms.Label();
+			this.FourThreeYear_RBTN = new System.Windows.Forms.RadioButton();
+			this.SelectYear_LBL = new System.Windows.Forms.Label();
 			this.Month_COMB = new System.Windows.Forms.ComboBox();
 			this.SelectMonth_LBL = new System.Windows.Forms.Label();
 			this.MonthlySalary_TB = new System.Windows.Forms.TextBox();
 			this.MonthlySalary_LBL = new System.Windows.Forms.Label();
-			this.SelectYear_LBL = new System.Windows.Forms.Label();
-			this.FourThreeYear_RBTN = new System.Windows.Forms.RadioButton();
-			this.NumberOfDays_LBL = new System.Windows.Forms.Label();
-			this.NumberOfDays_TB = new System.Windows.Forms.TextBox();
 			this.BackGround_PNL.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// BackGround_PNL
 			// 
 			this.BackGround_PNL.BackColor = System.Drawing.SystemColors.AppWorkspace;
+			this.BackGround_PNL.Controls.Add(this.NetPay_LBL);
+			this.BackGround_PNL.Controls.Add(this.NetTitle_LBL);
+			this.BackGround_PNL.Controls.Add(this.Marital_COMB);
+			this.BackGround_PNL.Controls.Add(this.Reset_BTN);
+			this.BackGround_PNL.Controls.Add(this.SalaryCalculation_BTN);
+			this.BackGround_PNL.Controls.Add(this.NumberOfCHildren_COMB);
+			this.BackGround_PNL.Controls.Add(this.NumberOfChildren_LBL);
+			this.BackGround_PNL.Controls.Add(this.MaritalStatus_LBL);
+			this.BackGround_PNL.Controls.Add(this.HiringDatePicker_DP);
+			this.BackGround_PNL.Controls.Add(this.HiringDate_LBL);
 			this.BackGround_PNL.Controls.Add(this.NumberOfDays_TB);
 			this.BackGround_PNL.Controls.Add(this.NumberOfDays_LBL);
 			this.BackGround_PNL.Controls.Add(this.FourThreeYear_RBTN);
@@ -52,10 +72,161 @@
 			this.BackGround_PNL.Controls.Add(this.SelectMonth_LBL);
 			this.BackGround_PNL.Controls.Add(this.MonthlySalary_TB);
 			this.BackGround_PNL.Controls.Add(this.MonthlySalary_LBL);
-			this.BackGround_PNL.Location = new System.Drawing.Point(28, 25);
+			this.BackGround_PNL.Location = new System.Drawing.Point(25, 24);
 			this.BackGround_PNL.Name = "BackGround_PNL";
 			this.BackGround_PNL.Size = new System.Drawing.Size(786, 531);
 			this.BackGround_PNL.TabIndex = 0;
+			// 
+			// NetPay_LBL
+			// 
+			this.NetPay_LBL.AutoSize = true;
+			this.NetPay_LBL.Location = new System.Drawing.Point(348, 394);
+			this.NetPay_LBL.Name = "NetPay_LBL";
+			this.NetPay_LBL.Size = new System.Drawing.Size(21, 25);
+			this.NetPay_LBL.TabIndex = 18;
+			this.NetPay_LBL.Text = "0";
+			// 
+			// NetTitle_LBL
+			// 
+			this.NetTitle_LBL.AutoSize = true;
+			this.NetTitle_LBL.Location = new System.Drawing.Point(182, 394);
+			this.NetTitle_LBL.Name = "NetTitle_LBL";
+			this.NetTitle_LBL.Size = new System.Drawing.Size(164, 25);
+			this.NetTitle_LBL.TabIndex = 17;
+			this.NetTitle_LBL.Text = "Net amount payable:";
+			// 
+			// Marital_COMB
+			// 
+			this.Marital_COMB.BackColor = System.Drawing.SystemColors.Info;
+			this.Marital_COMB.FormattingEnabled = true;
+			this.Marital_COMB.Items.AddRange(new object[] {
+            "Married",
+            "Single"});
+			this.Marital_COMB.Location = new System.Drawing.Point(353, 297);
+			this.Marital_COMB.Name = "Marital_COMB";
+			this.Marital_COMB.Size = new System.Drawing.Size(397, 33);
+			this.Marital_COMB.TabIndex = 16;
+			// 
+			// Reset_BTN
+			// 
+			this.Reset_BTN.BackColor = System.Drawing.SystemColors.Highlight;
+			this.Reset_BTN.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.Reset_BTN.Location = new System.Drawing.Point(353, 464);
+			this.Reset_BTN.Name = "Reset_BTN";
+			this.Reset_BTN.Size = new System.Drawing.Size(397, 30);
+			this.Reset_BTN.TabIndex = 15;
+			this.Reset_BTN.Text = "Reset";
+			this.Reset_BTN.UseVisualStyleBackColor = false;
+			this.Reset_BTN.Click += new System.EventHandler(this.Reset_BTN_Click);
+			// 
+			// SalaryCalculation_BTN
+			// 
+			this.SalaryCalculation_BTN.BackColor = System.Drawing.Color.SeaGreen;
+			this.SalaryCalculation_BTN.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.SalaryCalculation_BTN.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.SalaryCalculation_BTN.Location = new System.Drawing.Point(39, 427);
+			this.SalaryCalculation_BTN.Name = "SalaryCalculation_BTN";
+			this.SalaryCalculation_BTN.Size = new System.Drawing.Size(712, 30);
+			this.SalaryCalculation_BTN.TabIndex = 14;
+			this.SalaryCalculation_BTN.Text = "Salary calculation";
+			this.SalaryCalculation_BTN.UseVisualStyleBackColor = false;
+			this.SalaryCalculation_BTN.Click += new System.EventHandler(this.SalaryCalculation_BTN_Click);
+			// 
+			// NumberOfCHildren_COMB
+			// 
+			this.NumberOfCHildren_COMB.BackColor = System.Drawing.SystemColors.Info;
+			this.NumberOfCHildren_COMB.FormattingEnabled = true;
+			this.NumberOfCHildren_COMB.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+			this.NumberOfCHildren_COMB.Location = new System.Drawing.Point(353, 345);
+			this.NumberOfCHildren_COMB.Name = "NumberOfCHildren_COMB";
+			this.NumberOfCHildren_COMB.Size = new System.Drawing.Size(398, 33);
+			this.NumberOfCHildren_COMB.TabIndex = 13;
+			// 
+			// NumberOfChildren_LBL
+			// 
+			this.NumberOfChildren_LBL.AutoSize = true;
+			this.NumberOfChildren_LBL.Location = new System.Drawing.Point(34, 348);
+			this.NumberOfChildren_LBL.Name = "NumberOfChildren_LBL";
+			this.NumberOfChildren_LBL.Size = new System.Drawing.Size(312, 25);
+			this.NumberOfChildren_LBL.TabIndex = 12;
+			this.NumberOfChildren_LBL.Text = "Number of children under 18 years old *:";
+			// 
+			// MaritalStatus_LBL
+			// 
+			this.MaritalStatus_LBL.AutoSize = true;
+			this.MaritalStatus_LBL.Location = new System.Drawing.Point(34, 299);
+			this.MaritalStatus_LBL.Name = "MaritalStatus_LBL";
+			this.MaritalStatus_LBL.Size = new System.Drawing.Size(134, 25);
+			this.MaritalStatus_LBL.TabIndex = 10;
+			this.MaritalStatus_LBL.Text = "Marital status  *:";
+			// 
+			// HiringDatePicker_DP
+			// 
+			this.HiringDatePicker_DP.CalendarMonthBackground = System.Drawing.SystemColors.Info;
+			this.HiringDatePicker_DP.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.HiringDatePicker_DP.Location = new System.Drawing.Point(352, 245);
+			this.HiringDatePicker_DP.Name = "HiringDatePicker_DP";
+			this.HiringDatePicker_DP.Size = new System.Drawing.Size(399, 33);
+			this.HiringDatePicker_DP.TabIndex = 9;
+			// 
+			// HiringDate_LBL
+			// 
+			this.HiringDate_LBL.AutoSize = true;
+			this.HiringDate_LBL.Location = new System.Drawing.Point(34, 250);
+			this.HiringDate_LBL.Name = "HiringDate_LBL";
+			this.HiringDate_LBL.Size = new System.Drawing.Size(157, 25);
+			this.HiringDate_LBL.TabIndex = 8;
+			this.HiringDate_LBL.Text = "Select hiring date *:";
+			// 
+			// NumberOfDays_TB
+			// 
+			this.NumberOfDays_TB.BackColor = System.Drawing.SystemColors.Info;
+			this.NumberOfDays_TB.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.NumberOfDays_TB.Location = new System.Drawing.Point(352, 195);
+			this.NumberOfDays_TB.Name = "NumberOfDays_TB";
+			this.NumberOfDays_TB.Size = new System.Drawing.Size(399, 33);
+			this.NumberOfDays_TB.TabIndex = 7;
+			// 
+			// NumberOfDays_LBL
+			// 
+			this.NumberOfDays_LBL.AutoSize = true;
+			this.NumberOfDays_LBL.Location = new System.Drawing.Point(34, 198);
+			this.NumberOfDays_LBL.Name = "NumberOfDays_LBL";
+			this.NumberOfDays_LBL.Size = new System.Drawing.Size(205, 25);
+			this.NumberOfDays_LBL.TabIndex = 6;
+			this.NumberOfDays_LBL.Text = "Number of days worked *:";
+			// 
+			// FourThreeYear_RBTN
+			// 
+			this.FourThreeYear_RBTN.AutoSize = true;
+			this.FourThreeYear_RBTN.Checked = true;
+			this.FourThreeYear_RBTN.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FourThreeYear_RBTN.Location = new System.Drawing.Point(352, 154);
+			this.FourThreeYear_RBTN.Name = "FourThreeYear_RBTN";
+			this.FourThreeYear_RBTN.Size = new System.Drawing.Size(66, 29);
+			this.FourThreeYear_RBTN.TabIndex = 5;
+			this.FourThreeYear_RBTN.TabStop = true;
+			this.FourThreeYear_RBTN.Text = "1404";
+			this.FourThreeYear_RBTN.UseVisualStyleBackColor = true;
+			// 
+			// SelectYear_LBL
+			// 
+			this.SelectYear_LBL.AutoSize = true;
+			this.SelectYear_LBL.Location = new System.Drawing.Point(34, 154);
+			this.SelectYear_LBL.Name = "SelectYear_LBL";
+			this.SelectYear_LBL.Size = new System.Drawing.Size(111, 25);
+			this.SelectYear_LBL.TabIndex = 4;
+			this.SelectYear_LBL.Text = "Select year *:";
 			// 
 			// Month_COMB
 			// 
@@ -75,7 +246,7 @@
             "October (7th–8th - Mehr / Aban)",
             "November (8th–9th - Aban / Azar)",
             "December (9th–10th - Azar / Dey)"});
-			this.Month_COMB.Location = new System.Drawing.Point(342, 70);
+			this.Month_COMB.Location = new System.Drawing.Point(352, 105);
 			this.Month_COMB.Name = "Month_COMB";
 			this.Month_COMB.Size = new System.Drawing.Size(399, 33);
 			this.Month_COMB.TabIndex = 3;
@@ -84,7 +255,7 @@
 			// SelectMonth_LBL
 			// 
 			this.SelectMonth_LBL.AutoSize = true;
-			this.SelectMonth_LBL.Location = new System.Drawing.Point(24, 73);
+			this.SelectMonth_LBL.Location = new System.Drawing.Point(34, 108);
 			this.SelectMonth_LBL.Name = "SelectMonth_LBL";
 			this.SelectMonth_LBL.Size = new System.Drawing.Size(153, 25);
 			this.SelectMonth_LBL.TabIndex = 2;
@@ -94,7 +265,7 @@
 			// 
 			this.MonthlySalary_TB.BackColor = System.Drawing.SystemColors.Info;
 			this.MonthlySalary_TB.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.MonthlySalary_TB.Location = new System.Drawing.Point(342, 28);
+			this.MonthlySalary_TB.Location = new System.Drawing.Point(352, 55);
 			this.MonthlySalary_TB.Name = "MonthlySalary_TB";
 			this.MonthlySalary_TB.Size = new System.Drawing.Size(399, 33);
 			this.MonthlySalary_TB.TabIndex = 1;
@@ -102,50 +273,11 @@
 			// MonthlySalary_LBL
 			// 
 			this.MonthlySalary_LBL.AutoSize = true;
-			this.MonthlySalary_LBL.Location = new System.Drawing.Point(24, 31);
+			this.MonthlySalary_LBL.Location = new System.Drawing.Point(34, 58);
 			this.MonthlySalary_LBL.Name = "MonthlySalary_LBL";
-			this.MonthlySalary_LBL.Size = new System.Drawing.Size(319, 25);
+			this.MonthlySalary_LBL.Size = new System.Drawing.Size(283, 25);
 			this.MonthlySalary_LBL.TabIndex = 0;
-			this.MonthlySalary_LBL.Text = "Enter your monthly gross salary in Rials *:";
-			// 
-			// SelectYear_LBL
-			// 
-			this.SelectYear_LBL.AutoSize = true;
-			this.SelectYear_LBL.Location = new System.Drawing.Point(26, 124);
-			this.SelectYear_LBL.Name = "SelectYear_LBL";
-			this.SelectYear_LBL.Size = new System.Drawing.Size(111, 25);
-			this.SelectYear_LBL.TabIndex = 4;
-			this.SelectYear_LBL.Text = "Select year *:";
-			// 
-			// FourThreeYear_RBTN
-			// 
-			this.FourThreeYear_RBTN.AutoSize = true;
-			this.FourThreeYear_RBTN.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.FourThreeYear_RBTN.Location = new System.Drawing.Point(342, 124);
-			this.FourThreeYear_RBTN.Name = "FourThreeYear_RBTN";
-			this.FourThreeYear_RBTN.Size = new System.Drawing.Size(66, 29);
-			this.FourThreeYear_RBTN.TabIndex = 5;
-			this.FourThreeYear_RBTN.TabStop = true;
-			this.FourThreeYear_RBTN.Text = "1404";
-			this.FourThreeYear_RBTN.UseVisualStyleBackColor = true;
-			// 
-			// NumberOfDays_LBL
-			// 
-			this.NumberOfDays_LBL.AutoSize = true;
-			this.NumberOfDays_LBL.Location = new System.Drawing.Point(24, 164);
-			this.NumberOfDays_LBL.Name = "NumberOfDays_LBL";
-			this.NumberOfDays_LBL.Size = new System.Drawing.Size(205, 25);
-			this.NumberOfDays_LBL.TabIndex = 6;
-			this.NumberOfDays_LBL.Text = "Number of days worked *:";
-			// 
-			// NumberOfDays_TB
-			// 
-			this.NumberOfDays_TB.BackColor = System.Drawing.SystemColors.Info;
-			this.NumberOfDays_TB.Font = new System.Drawing.Font("IRANSansWeb", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.NumberOfDays_TB.Location = new System.Drawing.Point(342, 163);
-			this.NumberOfDays_TB.Name = "NumberOfDays_TB";
-			this.NumberOfDays_TB.Size = new System.Drawing.Size(399, 33);
-			this.NumberOfDays_TB.TabIndex = 7;
+			this.MonthlySalary_LBL.Text = "Basic monthly gross salary in Rials *:";
 			// 
 			// Dashboard_FRM
 			// 
@@ -179,5 +311,15 @@
 		private System.Windows.Forms.Label SelectYear_LBL;
 		private System.Windows.Forms.TextBox NumberOfDays_TB;
 		private System.Windows.Forms.Label NumberOfDays_LBL;
+		private System.Windows.Forms.DateTimePicker HiringDatePicker_DP;
+		private System.Windows.Forms.Label HiringDate_LBL;
+		private System.Windows.Forms.Label MaritalStatus_LBL;
+		private System.Windows.Forms.Button SalaryCalculation_BTN;
+		private System.Windows.Forms.ComboBox NumberOfCHildren_COMB;
+		private System.Windows.Forms.Label NumberOfChildren_LBL;
+		private System.Windows.Forms.Button Reset_BTN;
+		private System.Windows.Forms.ComboBox Marital_COMB;
+		private System.Windows.Forms.Label NetPay_LBL;
+		private System.Windows.Forms.Label NetTitle_LBL;
 	}
 }
--- SimpleSalaryTaxCalculator/SimpleSalaryTaxCalculator/Dashboard.cs
+++ SimpleSalaryTaxCalculator/SimpleSalaryTaxCalculator/Dashboard.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace SimpleSalaryTaxCalculator
 {
@@ -7,6 +8,92 @@ namespace SimpleSalaryTaxCalculator
 		public Dashboard_FRM()
 		{
 			InitializeComponent();
+		}
+
+		//Reset Button
+		private void Reset_BTN_Click(object sender, EventArgs e)
+		{
+			DialogResult result = MessageBox.Show("Do you want to clear the data?", "OK", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+			if (result == DialogResult.OK)
+			{
+				MonthlySalary_TB.Clear();
+				Month_COMB.SelectedIndex = 0;
+				FourThreeYear_RBTN.Checked = true;
+				NumberOfDays_TB.Clear();
+				HiringDatePicker_DP.Value = DateTime.Now;
+				Marital_COMB.SelectedIndex = -1;
+				NumberOfCHildren_COMB.SelectedIndex = -1;
+				NetPay_LBL.Text = "0";
+			}
+		}
+
+		// Calculate button
+		private void SalaryCalculation_BTN_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// Get inputs
+				decimal monthlySalary = decimal.Parse(MonthlySalary_TB.Text);
+				int monthIndex = Month_COMB.SelectedIndex + 1;
+				DateTime hireDate = HiringDatePicker_DP.Value;
+				bool isMarried = Marital_COMB.SelectedIndex == 0; // 0 = single, 1 = married
+				int childrenCount = int.Parse(NumberOfCHildren_COMB.SelectedItem.ToString());
+
+				// Number of days in a month
+				int daysInMonth = (monthIndex >= 1 && monthIndex <= 6) ? 31 : 30;
+
+				// Fixed values 1404
+				decimal minMonthlySalary = 103909680m; // Minimum monthly wage
+				decimal housingAllowance = 9000000m; // Housing allowance
+				decimal groceryAllowance = 22000000m; // Grocery bin
+				decimal marriageAllowance = isMarried ? 5000000m : 0m; // Marriage Allowance
+				decimal childAllowanceOne = 10390968m; // Child Allowance for one child
+				decimal seniorityBase = (hireDate.Year < 1403) ? 2820000m : 0m; // Seniority Base (at least 1 year of experience)
+
+				// Set base salary to legal minimum if needed
+				if (monthlySalary < minMonthlySalary)
+				{
+					monthlySalary = minMonthlySalary;
+					MessageBox.Show("The entered salary is less than the legal minimum. Minimum salary 1404 applied.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				}
+
+				// Calculate work experience
+				DateTime currentDate = new DateTime(1404, monthIndex, 1);
+				int yearsOfService = currentDate.Year - hireDate.Year;
+				if (currentDate.Month < hireDate.Month ||
+				(currentDate.Month == hireDate.Month && currentDate.Day < hireDate.Day))
+				{
+					yearsOfService--;
+				}
+
+				// Calculate child allowance based on number of children
+				decimal totalChildAllowance = childrenCount * childAllowanceOne;
+
+				// Calculate total fixed salary
+				decimal totalFixedSalary = monthlySalary +
+				housingAllowance +
+				groceryAllowance +
+				marriageAllowance +
+				totalChildAllowance +
+				seniorityBase;
+
+				// Show result
+				NetPay_LBL.Text = $"{totalFixedSalary:N0} Rials";
+
+				// Show details
+				string details = $"Base salary: {monthlySalary:N0}\n" +
+				$"Housing allowance: {housingAllowance:N0}\n" +
+				$"Grocery allowance: {groceryAllowance:N0}\n" +
+				$"Marriage allowance: {marriageAllowance:N0}\n" +
+				$"Child allowance: {totalChildAllowance:N0}\n" +
+				$"Seniority base: {seniorityBase:N0}\n" +
+				$"Total sum: {totalFixedSalary:N0}";
+				MessageBox.Show(details, "Salary details", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Please enter the inputs correctly.\nError: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 	}
 }
